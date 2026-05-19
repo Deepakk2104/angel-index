9638a523c16303b8c95b9616149daa213cf61e76
Deepak Kumar <deepakk12435@gmail.com>
2026-05-19 13:49:43 +0530

    Initial commit

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,30 @@
+export default function Navbar() {
+  return (
+    <nav className="top">
+      <div className="brand">
+        Angel
+        <span className="dot"></span>
+        <span className="index">Index</span>
+      </div>
+
+      <ul>
+        <li className="has-caret">Product</li>
+        <li className="has-caret">Solutions</li>
+        <li>Coverage</li>
+        <li>Pricing</li>
+        <li>Insights</li>
+        <li>Customers</li>
+      </ul>
+
+      <div className="nav-actions">
+        <a className="btn ghost" href="#">
+          Sign in
+        </a>
+
+        <a className="btn ink" href="#">
+          Request access <span className="arr">→</span>
+        </a>
+      </div>
+    </nav>
+  );
+}
